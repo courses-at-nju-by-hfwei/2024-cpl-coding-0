416c736747a04d499ba7e0c31bef3c356c622436
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2024-10-19 17:42:56 +0800

    4-loops: +code in class

--- 4-loops/game-of-life.c
+++ 4-loops/game-of-life.c
@@ -7,12 +7,12 @@
 #define SIZE 6
 
 const int board[SIZE][SIZE] = {
-    { 0 },
-    { 0, 1, 1, 0, 0, 0 },
-    { 0, 1, 1, 0, 0, 0 },
-    { 0, 0, 0, 1, 1, 0 },
-    { 0, 0, 0, 1, 1, 0 },
-    { 0 }
+    {0},
+    {0, 1, 1, 0, 0, 0},
+    {0, 1, 1, 0, 0, 0},
+    {0, 0, 0, 1, 1, 0},
+    {0, 0, 0, 1, 1, 0},
+    {0}
 };
 
 //const int board[SIZE][SIZE] = {
@@ -24,6 +24,71 @@ const int board[SIZE][SIZE] = {
 
 int main(void) {
   // TODO: play game-of-life
+
+  // expand the board
+  int old_board[SIZE + 2][SIZE + 2] = {0};
+  for (int i = 1; i <= SIZE; i++) {
+    for (int j = 1; j <= SIZE; ++j) {
+      old_board[i][j] = board[i - 1][j - 1];
+    }
+  }
+
+  for (int i = 1; i <= SIZE; i++) {
+    for (int j = 1; j <= SIZE; ++j) {
+      printf("%c ", old_board[i][j] ? '*' : ' ');
+    }
+    printf("\n");
+  }
+  system("clear");
+
+  // old board => new board by applying the rules
+  int new_board[SIZE + 2][SIZE + 2] = {0};
+
+  for (int round = 1; round <= 10; round++) {
+    for (int i = 1; i <= SIZE; ++i) {
+      for (int j = 1; j <= SIZE; ++j) {
+        // apply the rules:
+        // for each cell, count the number of live neighbours
+        int num_of_live_neighbours =
+            old_board[i - 1][j - 1] +
+            old_board[i - 1][j] +
+            old_board[i - 1][j + 1] +
+            old_board[i][j - 1] +
+            old_board[i][j + 1] +
+            old_board[i + 1][j - 1] +
+            old_board[i + 1][j] +
+            old_board[i + 1][j + 1];
+
+        if (old_board[i][j]) {
+          new_board[i][j] =
+              (num_of_live_neighbours == 2 || num_of_live_neighbours == 3);
+        } else {
+          new_board[i][j] = num_of_live_neighbours == 3;
+        }
+      }
+    }
+
+    for (int i = 1; i <= SIZE; i++) {
+      for (int j = 1; j <= SIZE; ++j) {
+        printf("%c ", new_board[i][j] ? '*' : ' ');
+      }
+      printf("\n");
+    }
+    // Linux: unistd.h
+    // Windows: windows.h (Sleep(ms))
+    sleep(1);
+
+    // Linux: stdlib.h
+    // Windows: stdlib.h (system("cls") clear screen)
+    system("clear");
+
+    // copy new_board to old_board
+    for (int i = 1; i <= SIZE; i++) {
+      for (int j = 1; j <= SIZE; ++j) {
+        old_board[i][j] = new_board[i][j];
+      }
+    }
+  }
 
   return 0;
 }
--- 4-loops/insertion-sort.c
+++ 4-loops/insertion-sort.c
@@ -6,10 +6,10 @@
 #include <time.h>
 
 #define MAX_LEN 10000
-#define RANGE 100
+#define RANGE 10
 
 int main(void) {
-  int numbers[MAX_LEN] = { 0 };
+  int numbers[MAX_LEN] = {0};
 
   int size = 0;
   scanf("%d", &size);
@@ -26,6 +26,25 @@ int main(void) {
   printf("\n");
 
   // TODO: insertion sort
+  for (int i = 1; i < size; i++) {
+    // numbers[0 .. i - 1] is already sorted
+    int key = numbers[i];
+
+    int j = i - 1;
+    while (j >= 0 && numbers[j] > key) {
+      numbers[j + 1] = numbers[j];
+      j--;
+    }
+
+    numbers[j + 1] = key;
+    // numbers[0 .. i] is already sorted
+
+    for (int i = 0; i < size; i++) {
+      printf("%d ", numbers[i]);
+    }
+    printf("\n");
+  }
+  // numbers[0 .. size - 1] is already sorted
 
   // print the sorted array
   for (int i = 0; i < size; i++) {
