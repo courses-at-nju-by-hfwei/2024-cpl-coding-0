90bd59a816bafcd0e500a139f22077cde067ec42
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2024-11-15 15:11:40 +0800

    +7-pointers-arrays template code

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -72,6 +72,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("D:/cpl/2024-cpl-coding-0/cmake-build-debug/6-data-types/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/cpl/2024-cpl-coding-0/cmake-build-debug/7-pointers-arrays/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
     set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
