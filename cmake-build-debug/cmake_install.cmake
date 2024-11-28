e1d0ef1fae7429ffe536866bdf2abfecd38e0c27
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2024-11-28 23:14:48 +0800

    +9-double-pointers template

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -82,6 +82,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("D:/cpl/2024-cpl-coding-0/cmake-build-debug/8-pointers-c-strings/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/cpl/2024-cpl-coding-0/cmake-build-debug/9-double-pointers/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
     set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
