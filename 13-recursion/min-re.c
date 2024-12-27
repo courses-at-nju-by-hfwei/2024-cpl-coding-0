875411f9c09eaa5faabde31ad40990bfaa7f9c4b
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2024-12-27 13:35:39 +0800

    +13-recursion template

---
+++ 13-recursion/min-re.c
@@ -0,0 +1,29 @@
+// Created by hfwei on 2024/12/25.
+
+// Visualization: https://tinyurl.com/min-re
+// (Using https://tinyurl.com/)
+
+#include <stdio.h>
+
+#define NUM 3
+const int numbers[NUM] = {65, 28, 37};
+
+int Min(const int nums[], int len);
+
+int main() {
+  int min = Min(numbers, NUM);
+
+  printf("min = %d\n", min);
+
+  return 0;
+}
+
+int Min(const int nums[], int len) {
+  if (len == 1) {
+    return nums[0];
+  }
+
+  int partial_min = Min(nums, len - 1);
+
+  return partial_min < nums[len - 1] ? partial_min : nums[len - 1];
+}
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -102,6 +102,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("D:/cpl/2024-cpl-coding-0/cmake-build-debug/12-linked-list/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/cpl/2024-cpl-coding-0/cmake-build-debug/13-recursion/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
     set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
