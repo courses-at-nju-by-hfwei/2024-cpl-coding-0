e06702d656abdc3adf73f15e2bfecf7b16837856
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2025-03-03 16:41:13 +0800

    +seu-recursion/

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -107,6 +107,17 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("D:/cpl/2024-cpl-coding-0/cmake-build-debug/13-recursion/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/cpl/2024-cpl-coding-0/cmake-build-debug/seu-recursion/cmake_install.cmake")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "D:/cpl/2024-cpl-coding-0/cmake-build-debug/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
 if(CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
     set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
@@ -120,8 +131,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
   file(WRITE "D:/cpl/2024-cpl-coding-0/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
---
+++ seu-recursion/stairs.c
@@ -0,0 +1,21 @@
+// Created by hfwei on 2025/03/03.
+// Without base case: https://tinyurl.com/stairs-wo-basecase
+// With base case: https://tinyurl.com/stairs-with-basecase
+// (Using https://tinyurl.com/)
+
+#include <stdio.h>
+
+void WalkdownStairsIteration(int n);
+
+int main(void) {
+  WalkdownStairsIteration(5);
+
+  return 0;
+}
+
+void WalkdownStairsIteration(int n) {
+  while (n > 1) {
+    printf("Walk down from %d to %d\n", n, n - 1);
+    n--;
+  }
+}
